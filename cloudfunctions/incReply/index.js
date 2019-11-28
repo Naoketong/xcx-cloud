6367e22d8693ca315d973cdf6fa5e98162e9eae3
xytdz20130731 <103790874@qq.com>
2019-11-28 11:14:20 +0800

    all

---
+++ cloudfunctions/incReply/index.js
@@ -0,0 +1,38 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+const _ = db.command
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let topic_id = event.topic_id;
+  try {
+    return await db.collection('topic').doc(topic_id).update({
+      data: {
+        reply_number: _.inc(1)
+      }
+    })
+  } catch(e) {
+    console.error(e)
+  }
+}
+
+// // 云函数入口文件
+// const cloud = require('wx-server-sdk')
+
+// cloud.init()
+
+// // 云函数入口函数
+// exports.main = async (event, context) => {
+//   const wxContext = cloud.getWXContext()
+
+//   return {
+//     event,
+//     openid: wxContext.OPENID,
+//     appid: wxContext.APPID,
+//     unionid: wxContext.UNIONID,
+//   }
+// }
+
+
